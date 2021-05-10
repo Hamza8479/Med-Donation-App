2a454adfb70c0dd739ee4184b87190057da6f657
Sameer Katija <sameerkatija@gmail.com>
2021-05-11 03:42:31 +0500

    added folders and setup the environment for front-end working

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  return <div className="App">App is working</div>;
 }
 
 export default App;
